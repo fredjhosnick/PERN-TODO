35b2668a1e842932c433708f02bb4a9fd30210bd
fredjhosnick <36901848+fredjhosnick@users.noreply.github.com>
2024-01-20 17:02:06 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user:"postgres",
-    password:"Hola@migo33",
+    password:"",
     host:"localhost",
     port:5432,
     database:"perntodo"
